229b6fbf8fd8e2cfcacf5b0786bbd520fbf5406d
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-01 21:52:12 +0000

    Initial Commit

---
+++ RandomUserGenerator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomUserGenerator
+//
+//  Created by Don Bouncy on 01/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUserGenerator/RandomUserGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserGeneratorApp.swift
+//  RandomUserGenerator
+//
+//  Created by Don Bouncy on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
